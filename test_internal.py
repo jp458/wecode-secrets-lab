42455cd46a1c0d0b1c8237d9a2eb890a9aa93ff3
Jean-Philippe Koudougnon <jean-philippe.koudougnon@ifsm.ci>
2026-07-03 17:13:53 +0000

    docs: add documentation example for internal key format

--- test_internal.py
+++ test_internal.py
@@ -1,2 +1,5 @@
 # Fichier de test pour clé interne
 INTERNAL_API_LOG = "WECODE-KEY-1A2B3C4D5E6F7G8H"
+
+# Exemple de documentation (Faux positif)
+# Dans vos tests, utilisez le format : WECODE-KEY-YOURKEYHERE0000
